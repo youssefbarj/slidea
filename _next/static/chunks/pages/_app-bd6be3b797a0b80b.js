82d9641c50af5a4cdcb4146e96c65280315f6a9c
youssefbarj <youssefbarj@users.noreply.github.com>
2026-05-12 17:00:04 +0000

    deploy: 5f70b4af405592f2641e62c06b5496d93db39747

---
+++ _next/static/chunks/pages/_app-bd6be3b797a0b80b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{2972:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(4948)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(2972),n(6978))),_N_E=_.O()}]);
---
+++ _next/static/chunks/pages/_error-5f8767fcfc3483d8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{490:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(8105)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(490)),_N_E=_.O()}]);
